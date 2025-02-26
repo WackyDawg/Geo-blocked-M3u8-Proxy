617bf7a88f553f767a47477bcb5ae6e402c642d7
WackyDawg <juliannwadinobi098@gmail.com>
2025-02-26 05:28:17 +0100

    Refactor proxy middleware to use HTTPS and handle errors more gracefully

--- src/middlewares/proxyStream.js
+++ src/middlewares/proxyStream.js
@@ -1,7 +1,7 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-const config = require("../config/config.json");
+const https = require("https");
+const config = require("../config.json");
 
-module.exports = (req, res, next) => {
+module.exports = (req, res) => {
   const streamId = req.params.streamId;
   const targetUrl = config.streams[streamId];
 
@@ -9,14 +9,14 @@ module.exports = (req, res, next) => {
     return res.status(404).json({ error: "Stream not found" });
   }
 
-  createProxyMiddleware({
-    target: targetUrl,
-    changeOrigin: true,
-    secure: false,
-    headers: {
-      "User-Agent": "Mozilla/5.0",
-      Referer: "https://google.com"
-    },
-    pathRewrite: { [`^/stream/${streamId}`]: "" },
-  })(req, res, next);
+  https.get(targetUrl, (proxyRes) => {
+    if (proxyRes.statusCode !== 200) {
+      return res.status(proxyRes.statusCode).json({ error: "Access Denied" });
+    }
+
+    res.setHeader("Content-Type", "application/vnd.apple.mpegurl");
+    proxyRes.pipe(res);
+  }).on("error", (err) => {
+    res.status(500).json({ error: "Proxy request failed", details: err.message });
+  });
 };
