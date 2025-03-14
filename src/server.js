6196484ef49d3984617cb84a4c194baa81a85aae
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2025-03-14 21:53:33 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -4,21 +4,17 @@ const { Parser } = require('m3u8-parser');
 const app = express();
 const PORT = 3000;
 
-// Allow CORS for HLS player testing
 app.use(require('cors')());
 
-// Proxy configuration
+
 const ORIGIN_URL = 'https://adultswim-vodlive.cdn.turner.com/live/rick-and-morty/stream_de.m3u8'; // Original stream URL
 const PROXY_URL = 'https://geo-blocked-m3u8-proxy.onrender.com';
 
-// Proxy .m3u8 playlists
 app.get('/*.m3u8', async (req, res) => {
   try {
-    // Fetch original playlist
     const response = await axios.get(`${ORIGIN_URL}/${req.params[0]}.m3u8`);
     const playlist = response.data;
 
-    // Parse and rewrite URIs
     const parser = new Parser();
     parser.push(playlist);
     parser.end();
@@ -40,7 +36,6 @@ app.get('/*.m3u8', async (req, res) => {
   }
 });
 
-// Proxy .ts segments
 app.get('/*.ts', async (req, res) => {
   try {
     // Fetch and pipe original segment
@@ -50,7 +45,6 @@ app.get('/*.ts', async (req, res) => {
       responseType: 'stream'
     });
 
-    // Forward headers and stream
     res.header('Content-Type', 'video/MP2T');
     response.data.pipe(res);
   } catch (error) {
